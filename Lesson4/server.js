74d1c8b2ad996c457d97ef5901e5b5ce819d4de5
Rakibul-Hasan <rakib.it.ergon@gmail.com>
2025-09-27 01:03:15 +0600

    http res, req model and status code

---
+++ Lesson4/server.js
@@ -0,0 +1,17 @@
+// Module | http module - create http server
+
+const http =require('http');
+const port = 3000;
+const hostname = '127.0.0.1'
+
+const myServer = http.createServer((req, res)=>{
+    res.writeHead(202, {'content-type': 'text/html'});
+    res.write("<h1>Hello </h1>")
+    res.write("<h1>Hello </h1>")
+    res.write("<h1>Hello </h1>")
+    res.end();
+});
+
+myServer.listen(port, hostname, ()=>{
+    console.log(`server is running successfully at http://${hostname}:${port}`);
+});
