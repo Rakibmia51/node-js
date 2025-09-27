4bae68c60086cad5bdddd210b5a693f36fe535b0
Rakibul-Hasan <rakib.it.ergon@gmail.com>
2025-09-28 01:56:48 +0600

    http methods and postman

--- Lesson8/app.js
+++ Lesson8/app.js
@@ -1,4 +1,24 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res)=>{
+    res.send("I am a get request at home router");
+    res.end();
+});
+
+app.post("/", (req, res)=>{
+    res.send("I am a Post request at Post route");
+    res.end();
+});
+
+app.put("/", (req, res)=>{
+    res.send("I am a put request at Post route");
+    res.end();
+});
+
+app.delete("/", (req, res)=>{
+    res.send("I am a delete request at Post route");
+    res.end();
+});
+
 module.exports = app;
