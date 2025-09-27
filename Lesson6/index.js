2b8477490e0bf69ce9d943fbf1b0b3eb43d11020
Rakibul-Hasan <rakib.it.ergon@gmail.com>
2025-09-27 11:16:43 +0600

    http routing and nodemon package

---
+++ Lesson6/index.js
@@ -0,0 +1,52 @@
+// Learning Oucomes
+
+// - What is routing? How to do it?
+// - Installing nodemon package
+// - How to get the requested url
+// - Reading html files in server based on requested url
+
+
+// step: 01 server create
+// step: 02 nodemon package init
+// step: 03 Go to (package.json ) then scripts - "start": "nodemon index.js"
+
+
+
+const http = require('http');
+const fs = require('fs');
+const PORT = 3000;
+const hostName = '127.0.0.1';
+
+
+const server = http.createServer((req, res)=>{
+    // console.log(req.url);
+    // res.end("Welcome to the server");
+
+    const handleReadFile = (statusCode, fileLocation)=>{
+        fs.readFile(fileLocation, (err, data)=>{
+            res.writeHead(statusCode, {"content-type": "text/html"});
+            res.write(data);
+            res.end();
+        })
+    }
+
+    if(req.url === '/'){
+            handleReadFile(200, "./views/index.html")
+    }
+
+    else if(req.url === '/about'){
+            handleReadFile(200, "./views/about.html")
+    }
+
+    else if(req.url === '/contact'){
+        handleReadFile(200, "./views/contact.html")
+    }
+
+    else {
+         handleReadFile(200, "./views/error.html")
+    }
+});
+
+server.listen(PORT, hostName, ()=>{
+    console.log(`Server is running at http://${hostName}:${PORT}`);
+});
