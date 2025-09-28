f539c9f3421287c90ee262b9a01ededc8b3e9414
Rakibul-Hasan <rakib.it.ergon@gmail.com>
2025-09-28 13:05:08 +0600

    Express - http response | html file, json data

--- Lesson8/app.js
+++ Lesson8/app.js
@@ -5,9 +5,32 @@ const userRouter = require('./routes/users.route');
 // Middleware
 app.use("/api/user",userRouter);
 
+app.use("/register", (req, res)=>{
+    // res.status(200).json({
+    //     message: "I am register page",
+    //     statusCode: 200,
+    // });
+    // res.redirect("/login")
+
+    res.statusCode = 200;
+    res.sendFile(__dirname+"/views/register.html");
+});
+
+app.use("/login", (req, res)=>{
+    // res.send("hi I am login page");
+    // res.cookie("name", "rakibul");
+    // res.cookie("age", "30" )
+    res.clearCookie("name");
+    res.append("id", "130000");
+    res.end();
+})
+
 // Routes Home / error
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from Express App Home Route 🚀</h1>');
+  res.statusCode= 200;
+  res.sendFile(__dirname+"/views/index.html");
+
+//   res.send('<h1>Hello from Express App Home Route 🚀</h1>');
 });
 
 app.use((req, res)=>{
