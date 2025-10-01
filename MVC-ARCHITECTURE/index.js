c44a3ac5786abb38c89193749ba7496faad18cf2
Rakibul-Hasan <rakib.it.ergon@gmail.com>
2025-10-01 09:52:47 +0600

    No follow MVC Architecture

--- MVC-ARCHITECTURE/index.js
+++ MVC-ARCHITECTURE/index.js
@@ -2,8 +2,48 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.urlencoded({extended:true}))
+
+const users = [
+    {
+        name: "Rakibul Hasan",
+        age: 27
+    },
+    {
+        name: "Sakibul Hasan",
+        age: 22
+    }
+]
+
+const htmlForm = `
+    <form method="POST" action ="/users">
+        <input type="text" name ="name" placeholder="Enter Name"/>
+
+        <input type="number" name ="age" placeholder="Enter age"/>
+
+        <button type="submit">Save user</button>
+
+    </form>
+`
+
 app.get("/users", (req, res)=>{
-    res.send("Hello Users")
+   res.send(htmlForm)
+})
+
+app.post("/users", (req, res)=>{
+    // res.send(htmlForm)
+
+    const name = req.body.name;
+   const age = Number(req.body.age);
+   const user = {
+    name,
+    age
+   }
+   users.push(user)
+   res.status(201).json({
+    success: true,
+    users
+   })
 })
 
 app.listen(PORT, ()=>{
