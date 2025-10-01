3a9b88acddb508aa15f0965f50600d81f9543a41
Rakibul-Hasan <rakib.it.ergon@gmail.com>
2025-10-01 23:07:11 +0600

    MVC-practice

---
+++ MVC-practice/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const userRouter = require('./routes/users.route');
+
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.urlencoded({extended:true}));
+app.use(userRouter); // Users
+
+
+
+
+
+app.use((req, res, next)=>{
+    res.status(404).json({
+        message: "Data not Found 404 Error",
+    })
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
