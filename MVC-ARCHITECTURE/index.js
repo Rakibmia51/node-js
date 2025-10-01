dcbec5b029da801331dc17aa4c414b7a15d648a9
Rakibul-Hasan <rakib.it.ergon@gmail.com>
2025-10-01 18:17:41 +0600

    MVC-ARCHITECTURE wise

--- MVC-ARCHITECTURE/index.js
+++ MVC-ARCHITECTURE/index.js
@@ -1,50 +1,21 @@
 const express = require('express');
+
+const userRouter = require('./routes/users.route');
+const productRouter = require('./routes/products.route');
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({extended:true}))
+app.use(userRouter);
+app.use(productRouter);
 
-const users = [
-    {
-        name: "Rakibul Hasan",
-        age: 27
-    },
-    {
-        name: "Sakibul Hasan",
-        age: 22
-    }
-]
+app.use((req, res, next)=>{
+    res.status(404).json({
+        message: "resource not found",
+    });
+});
 
-const htmlForm = `
-    <form method="POST" action ="/users">
-        <input type="text" name ="name" placeholder="Enter Name"/>
-
-        <input type="number" name ="age" placeholder="Enter age"/>
-
-        <button type="submit">Save user</button>
-
-    </form>
-`
-
-app.get("/users", (req, res)=>{
-   res.send(htmlForm)
-})
-
-app.post("/users", (req, res)=>{
-    // res.send(htmlForm)
-
-    const name = req.body.name;
-   const age = Number(req.body.age);
-   const user = {
-    name,
-    age
-   }
-   users.push(user)
-   res.status(201).json({
-    success: true,
-    users
-   })
-})
 
 app.listen(PORT, ()=>{
     console.log(`server is running http://localhost:${PORT}`)
---
+++ MVC-ARCHITECTURE/models/products.model.js
@@ -0,0 +1,12 @@
+const products = [
+    {
+        name: "Dicare",
+        price: 375
+    },
+    {
+        name: "E-opi",
+        price: 900
+    },
+];
+
+module.exports = products;
