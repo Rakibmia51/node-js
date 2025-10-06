242cbee570ba266971221543f6e26cffadc5abb6
Rakibul-Hasan <rakib.it.ergon@gmail.com>
2025-10-07 01:20:38 +0600

    Rest-Api-MongoBd-MVC 03

--- Rest-Api-MongoBd-MVC/app.js
+++ Rest-Api-MongoBd-MVC/app.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+require("./config/db");
+
+const userRouter = require("./routes/user.route");
 
 const app = express();
 
 app.use(cors());
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use("/api/users",userRouter);
 
 // api/users : GET
 // api/users/:id : GET
 // api/users/: POST
-// api/users/: id :
-//  PATCH
+// api/users/: id : PATCH
 // api/users/: id : DELETE
-
-
 
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/./views/index.html")
