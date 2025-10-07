a20e574d1d1bda1de3fc520930c94226a29e3269
Rakibul-Hasan <rakib.it.ergon@gmail.com>
2025-10-07 21:51:43 +0600

    layout EJS

--- EJS-Tutorial/Lesson1/index.js
+++ EJS-Tutorial/Lesson1/index.js
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
   res.render("index", {plNames: pLanguages})
 })
 
+app.get('/contact', (req, res) => {
+  res.render("contact", {})
+})
+
 app.post('/', (req, res) => {
   const pLanguage = req.body.pLanguage;
     pLanguages.push(pLanguage);
