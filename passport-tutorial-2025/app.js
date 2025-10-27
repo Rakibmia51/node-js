3845b8df7f9814f5f981633f63e10334d26bb937
Rakibul-Hasan <rakib.it.ergon@gmail.com>
2025-10-27 20:20:49 +0600

    Authentication | Passport local strategy session base

--- passport-tutorial-2025/app.js
+++ passport-tutorial-2025/app.js
@@ -1,13 +1,43 @@
 
 const express = require('express')
 const cors = require('cors')
-const ejs = require('ejs')
+const ejs = require('ejs');
+const User = require('./models/user.model');
 const app = express()
+require("./config/database");
+require("./config//passport");
+require("dotenv").config();
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
+
 
 app.set("view engine", "ejs");
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({
+          mongoUrl: process.env.MONGO_URL,
+          collectionName: "sessions"
+        })
+      // cookie: { secure: true }
+      })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 // base url - home route
 app.get('/', (req, res) => {
@@ -20,23 +50,74 @@ app.get('/register', (req, res) => {
 })
 
 // register : post
-app.post('/register', (req, res) => {
- 
+app.post('/register', async (req, res) => {
+    try {
+      const user = await User.findOne({
+        username: req.body.username
+      })
+      if(user) return res.status(400).send("User already exists")
+          bcrypt.hash(req.body.password, saltRounds, async(err, hash)=> {
+
+              const newUser = new User({
+                username: req.body.username,
+                password: hash
+              });
+              await newUser.save()
+              res.redirect("/login");
+            
+          });
+        
+    } catch (error) {
+      
+    }
 })
 
+
+
+const checkLogged = (req, res, next)=>{
+  if(req.isAuthenticated()){
+    return res.redirect("/profile")
+  }
+  next();
+}
 // login : get
-app.get('/login', (req, res) => {
+app.get('/login', checkLogged, (req, res) => {
   res.render("login")
 })
 
 // login : post
+app.post('/login', 
+  passport.authenticate('local',{ 
+    failureRedirect: '/login',
+    successRedirect: "/profile"
+   })
+  );
 
 
+  const checkAuthenticated = (req, res, next)=>{
+  if(req.isAuthenticated()){
+    return next()
+  }
+  res.redirect("/login")
+}
 // profile protected route
-app.get('/profile', (req, res) => {
+app.get('/profile', checkAuthenticated, (req, res) => {
   res.render("profile")
 })
 
+// Logout route
+app.get('/logout', (req, res) => {
+  try {
+    req.logOut((err)=>{
+      if(err){
+        return next(err);
+      }
+      res.redirect("/")
+    })
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+})
 
 
 module.exports = app;
---
+++ passport-tutorial-2025/config/passport.js
@@ -0,0 +1,39 @@
+
+const passport = require("passport")
+const bcrypt = require("bcrypt")
+const LocalStrategy = require("passport-local");
+const User = require("../models/user.model");
+
+passport.use(
+    new LocalStrategy( async (username, password, done) => {
+        try {
+            const user = await User.findOne({username: username})
+            if (!user) { 
+                return done(null, false),{message: "Incorrect Username"} 
+                }
+                
+            if(!bcrypt.compare(password, user.password)){
+                return done(null, false),{message: "Incorrect Password"} 
+            }
+                return done(null, user);
+            } catch (error) {
+            return done(error);
+        }
+  }
+));
+
+// create session id
+// whenever we login it creares user id inside session
+passport.serializeUser((user, done)=>{
+    done(null, user.id);
+});
+
+// find session info using session id
+passport.deserializeUser(async (id, done)=>{
+    try {
+        const user = await User.findById(id);
+        done(null, user)
+    } catch (error) {
+        done(error, false)
+    }
+})
